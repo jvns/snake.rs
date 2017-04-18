19f014871ffb307d89c21989eef7c5740bf2e2d9
Julia Evans <julia@jvns.ca>
2017-04-18 00:36:45 -0400

    refactor some things

--- src/main.rs
+++ src/main.rs
@@ -43,66 +43,72 @@ impl Board {
     return Point{x: (xrand % self.xmax) as i32, y: (yrand % self.ymax) as i32};
   }
 
-  fn initialize_snake(&mut self) {
+  fn initialize(&mut self) {
     self.snake.push(Point{x: 2, y: 3});
     self.snake.push(Point{x: 2, y: 2});
-  }
-}
-
-
-
-fn next_move(board: &Board, dir: Direction) -> Result<Point, ()> {
-  let snake = &board.snake;
-  let head = &snake[0];
-  let mut new_x = head.x;
-  let mut new_y = head.y;
-  match dir {
-    Direction::UP => {
-      new_y -= 1;
-    }
-    Direction::DOWN => {
-      new_y += 1;
-    }
-    Direction::RIGHT => {
-      new_x += 1;
-    }
-    Direction::LEFT => {
-      new_x -= 1;
+    // 1 food per 150 squares on the board
+    let num_food = self.xmax * self.ymax / 150;
+    for _ in 1..num_food {
+      self.add_new_food();
     }
   }
-  if new_x < 0 || new_y < 0 || new_x >= board.xmax as i32 || new_y >= board.ymax as i32 {
-    return Err(());
-  } else {
-    return Ok(Point{x: new_x, y: new_y});
-  }
-}
 
-fn move_snake(board: &mut Board, dir: Direction) -> Status {
-   let beginning = next_move(board, dir);
-   match beginning {
-     Err(_) => {return Status::FAILURE}
-     Ok(_) => {}
-   }
-   let point: Point = beginning.unwrap();
-
-   // if we're going backwards, ignore and move on
-   if board.snake[1] == point {
+  fn move_snake(&mut self, dir: Direction) -> Status {
+    let beginning = self.next_move(dir);
+    match beginning {
+      Err(_) => {return Status::FAILURE}
+      Ok(_) => {}
+    }
+    let point: Point = beginning.unwrap();
+    // if we're going backwards, ignore and move on
+    if self.snake[1] == point {
+     return Status::SUCCESS;
+    }
+    // Check for collisions!
+    if self.snake.contains(&point) {
+     return Status::FAILURE;
+    }
+    if self.foods.contains(&point) {
+     self.eat_food(point);
+     self.foods.retain(|&x| x != point);
+     self.add_new_food();
+     return Status::SUCCESS;
+    }
+    self.move_to(point);
     return Status::SUCCESS;
-   }
-   // Check for collisions!
-   if board.snake.contains(&point) {
-    return Status::FAILURE;
-   }
-   if board.foods.contains(&point) {
-    board.eat_food(point);
-//     remove_from_list(beginning, &(board->foods));
-    board.foods.retain(|&x| x != point);
-    board.add_new_food();
-    return Status::SUCCESS;
-   }
-   board.move_to(point);
-   return Status::SUCCESS;
+  }
+
+  fn next_move(&self, dir: Direction) -> Result<Point, ()> {
+    let head = &self.snake[0];
+    let mut new_x = head.x;
+    let mut new_y = head.y;
+    match dir {
+      Direction::UP => {
+        new_y -= 1;
+      }
+      Direction::DOWN => {
+        new_y += 1;
+      }
+      Direction::RIGHT => {
+        new_x += 1;
+      }
+      Direction::LEFT => {
+        new_x -= 1;
+      }
+    }
+    if new_x < 0 || new_y < 0 || new_x >= self.xmax as i32 || new_y >= self.ymax as i32 {
+      return Err(());
+    } else {
+      return Ok(Point{x: new_x, y: new_y});
+    }
+  }
+
 }
+
+
+
+
+
 
 
 fn display_points(snake: &Vec<Point>, symbol: chtype) {
@@ -146,11 +152,7 @@ fn main() {
   let mut dir = Direction::RIGHT;
 
   let mut board = Board{xmax: xmax as u32, ymax: ymax as u32, foods: vec!(), snake: vec!()};
-  board.initialize_snake();
-
-  for _ in 1..15 {
-    board.add_new_food();
-  }
+  board.initialize();
 
   loop {
     clear();
@@ -158,7 +160,7 @@ fn main() {
     display_points(&board.foods, ACS_DIAMOND());
     refresh();
     dir = get_next_move(dir.clone());
-    let status = move_snake(&mut board, dir);
+    let status = board.move_snake(dir);
     if  status == Status::FAILURE{
       break
     }
